b618381c7bbb0dc3a32318369cb1be5072382a6d
idrisskc <idriss.choudjem@gmail.com>
2022-10-16 12:39:55 -0700

    fc

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 import os
 from app import create_app
+from waitress import serve
 
 app = create_app()
 
@@ -7,6 +8,7 @@ app = create_app()
 def index():
     return '<h1>Hello!</h1>'
 
+# serve(app, host='0.0.0.0', port=8080, threads=1)
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run() 
